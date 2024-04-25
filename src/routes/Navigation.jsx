ab5edfd1c671d561fd100671be68076e8ca9918f
jdanigh03 <dani151203gh@gmail.com>
2024-04-25 10:37:28 -0400

    browser

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from '../Home';
 import CardRick from '../CardRick';
 
 const Navigation = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/social-links-profile' element={<Home />} />
         <Route path='/character' element={<CardRick />} />
         <Route path="/*" element={<p>Error 404</p>}/>
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
