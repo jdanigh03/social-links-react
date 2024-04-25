504108a23a26e697a84b798ab6f4b65c43243034
jdanigh03 <dani151203gh@gmail.com>
2024-04-25 07:32:35 -0400

    puat1}

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./Home.jsx";
+import RickAndMortyApp from "./RickAndMortyApp.jsx";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/character",
+    element: <RickAndMortyApp />,
+  },
+  {
+    path: "/*",
+    element: <p>ERROR 404 Not found</p>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
